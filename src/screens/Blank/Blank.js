4455d6d9e6840029b1a3d6d17c70fa853e4cda55
Drzaln <drzaln@null.net>
2021-01-16 19:34:05 +0700

    detail screen done

--- src/screens/Blank/Blank.js
+++ src/screens/Blank/Blank.js
@@ -1,10 +1,6 @@
-import React, { useMemo } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Text } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
-import Svg, { Circle } from 'react-native-svg'
-import Animated, { interpolate, multiply, concat, set, useCode } from 'react-native-reanimated'
-import { clamp, timing, toRad, useValue } from 'react-native-redash/lib/module/v1'
-import AnimateableText from 'react-native-animateable-text'
 
 const Blank = () => {
 	return (
@@ -20,93 +16,8 @@ const Blank = () => {
 			showsHorizontalScrollIndicator={false}
 			showsVerticalScrollIndicator={false}>
 			<Text style={{ fontFamily: 'JuventusFans-Bold', color: '#3E4346', fontSize: 64 }}>J</Text>
-			{/* <CircleComponent percent={progress} /> */}
 		</ScrollView>
 	)
 }
 
 export default Blank
-
-//DISINI
-
-const AnimatedCircle = Animated.createAnimatedComponent(Circle)
-const AnimatedSVG = Animated.createAnimatedComponent(Svg)
-
-const styles = StyleSheet.create({
-	container: {
-		justifyContent: 'center',
-		alignItems: 'center',
-		transform: [ { rotate: '-90deg' } ]
-	}
-})
-
-const CircleComponent = ({
-	percent = 75,
-	duration = 10000,
-	bgStrokeColor = '#3E4346',
-	radius = 40,
-	isRadius = false,
-	strokeColor = '#FFC93C',
-	strokeWidth = 6,
-	maxProgress = 100,
-	minProgress = 0
-}) => {
-	const progressAnimated = useValue(0)
-	const progressSpin = useValue(0)
-	const actualProgress = clamp(progressAnimated, minProgress, maxProgress)
-
-	//effect
-	useCode(() => [ set(progressAnimated, timing({ from: progressAnimated, to: percent, duration })) ], [ percent ])
-	useCode(() => [ set(progressSpin, timing({ from: progressSpin, to: percent })) ], [])
-
-	// variable
-	const strokeDasharray = useMemo(() => `${radius * 2 * Math.PI} ${radius * 2 * Math.PI}`, [ radius ])
-	const alpha = interpolate(actualProgress, {
-		inputRange: [ minProgress, maxProgress ],
-		outputRange: [ Math.PI * 2, 0 ]
-	})
-	const strokeDashoffset = multiply(alpha, radius)
-
-	// style
-	const svgStyle = [
-		{
-			transform: [
-				{ rotate: interpolate(progressSpin, { inputRange: [ 0, 1 ], outputRange: [ toRad(0), Math.PI * 2 ] }) }
-			]
-		}
-	]
-	return (
-		<View style={[ styles.container ]}>
-			<AnimatedSVG width={radius * 2 + strokeWidth} height={radius * 2 + strokeWidth} style={svgStyle}>
-				<AnimatedCircle
-					r={radius}
-					x={radius + strokeWidth / 2}
-					y={radius + strokeWidth / 2}
-					stroke={bgStrokeColor}
-					strokeWidth={strokeWidth}
-				/>
-				<AnimatedCircle
-					strokeLinecap={isRadius ? 'round' : undefined}
-					strokeDashoffset={strokeDashoffset}
-					strokeDasharray={strokeDasharray}
-					r={radius}
-					x={radius + strokeWidth / 2}
-					y={radius + strokeWidth / 2}
-					stroke={strokeColor}
-					strokeWidth={strokeWidth}
-				/>
-			</AnimatedSVG>
-			<AnimateableText
-				text={concat(actualProgress, '%')}
-				style={{
-					fontFamily: 'Oswald-Medium',
-					color: '#FFC93C',
-					fontSize: 18,
-					textAlign: 'center',
-					position: 'absolute',
-					transform: [ { rotate: '90deg' } ]
-				}}
-			/>
-		</View>
-	)
-}
--- src/screens/Detail/Detail.js
+++ src/screens/Detail/Detail.js
@@ -1,12 +1,167 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, Image, useWindowDimensions } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import LinearGradient from 'react-native-linear-gradient'
+import CircleAnimated from '../../components/CircleAnimated/CircleAnimated'
+import TopNavigation from '../../components/TopNavigation/TopNavigation'
 
 const Detail = () => {
-    return (
-        <View>
-            <Text>Detail</Text>
-        </View>
-    )
+	const [ selectedNav, setSelectedNav ] = useState(0)
+	const nav = [ 'defence', 'distribution', 'attack', 'discipline' ]
+	return (
+		<ScrollView
+			contentContainerStyle={{ paddingVertical: 16, backgroundColor: '#030610' }}
+			overScrollMode='never'
+			showsHorizontalScrollIndicator={false}
+			showsVerticalScrollIndicator={false}>
+			<View style={{ flexDirection: 'row', paddingHorizontal: 16 }}>
+				<View style={{ flex: 1, justifyContent: 'space-between' }}>
+					<PlayerInfo />
+					<NumberStat />
+					<NumberStat number={64} text='SHOTS ON TARGET' />
+					<NumberStat direction='column' number={2.239} text='MINUTES PLAYED' />
+				</View>
+				<BigImage />
+			</View>
+			<TopNavigation
+				nav={nav}
+				selectedNav={selectedNav}
+				mode='blokline'
+				style={{ marginVertical: 16, marginLeft: 16 }}
+				onPress={(key) => setSelectedNav(key)}
+			/>
+			<View style={{ marginHorizontal: 16, flexDirection: 'row', justifyContent: 'space-between' }}>
+				<PlayerStat percent={75} number={26} title='Clearance' barTitle='Tackles' />
+				<PlayerStat percent={48.1} number={2} title='Blocks' barTitle='Duel' />
+				<PlayerStat percent={44.1} number={4} title='Interceptions' barTitle='Aerial Duels' />
+			</View>
+		</ScrollView>
+	)
 }
 
 export default Detail
+
+const PlayerStat = ({ percent, number, title, barTitle }) => {
+	return (
+		<View style={{ justifyContent: 'center', alignItems: 'center' }}>
+			<Text
+				style={{
+					fontSize: 18,
+					lineHeight: 24,
+					color: '#FFC93C'
+				}}>
+				{number}
+			</Text>
+			<Text
+				style={{
+					fontFamily: 'Oswald-Regular',
+					fontSize: 12,
+					lineHeight: 17,
+					color: '#3E4346'
+				}}>
+				{title}
+			</Text>
+			<Text
+				style={{
+					fontSize: 12,
+					lineHeight: 17,
+					color: '#FAFAFA',
+					marginBottom: 8,
+					marginTop: 16
+				}}>
+				{barTitle}
+			</Text>
+			<CircleAnimated percent={percent} duration={1700} />
+		</View>
+	)
+}
+
+const PlayerInfo = () => {
+	return (
+		<View>
+			<Text
+				style={{
+					fontFamily: 'JuventusFans-Bold',
+					fontSize: 96,
+					color: '#25282A',
+					position: 'absolute',
+					top: -45
+				}}>
+				7
+			</Text>
+			<Text
+				style={{
+					fontFamily: 'Open Sans',
+					fontSize: 24,
+					lineHeight: 33,
+					fontWeight: 'bold',
+					color: '#FAFAFA'
+				}}>
+				Cristiano RONALDO
+			</Text>
+			<Text
+				style={{
+					fontFamily: 'Open Sans',
+					fontSize: 18,
+					lineHeight: 24,
+					fontWeight: '600',
+					color: '#FFC93C'
+				}}>
+				Forward
+			</Text>
+		</View>
+	)
+}
+
+const BigImage = () => {
+	const { width } = useWindowDimensions()
+	const imageHeight = width - 150
+	return (
+		<View style={{ flex: 1.2, overflow: 'hidden' }}>
+			<View style={{ height: imageHeight, width: '100%' }}>
+				<Image
+					style={{ height: '165%', width: '100%' }}
+					source={require('../../assets/team/RONALDO_970x700.png')}
+					resizeMethod='auto'
+					resizeMode='cover'
+				/>
+				<LinearGradient
+					colors={[ 'transparent', '#030610' ]}
+					style={{
+						height: 30,
+						width: '100%',
+						position: 'absolute',
+						bottom: 0
+					}}
+				/>
+			</View>
+		</View>
+	)
+}
+
+const NumberStat = ({ number = 19, text = 'GOALS', direction = 'row' }) => {
+	return (
+		<View style={{ flexDirection: direction, alignItems: direction === 'row' ? 'center' : undefined }}>
+			<Text
+				style={{
+					fontFamily: 'Open Sans',
+					fontSize: 24,
+					lineHeight: 33,
+					fontWeight: 'bold',
+					color: '#FAFAFA'
+				}}>
+				{number}
+			</Text>
+			<Text
+				style={{
+					fontFamily: 'Oswald-SemiBold',
+					fontSize: 12,
+					lineHeight: 17,
+					color: '#3E4346',
+					marginLeft: direction === 'row' ? 4 : undefined
+				}}>
+				{text}
+			</Text>
+		</View>
+	)
+}
